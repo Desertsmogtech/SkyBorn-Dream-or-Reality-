1069e0899d7d82818576adc6375c03978bf11232
Jason VanZile <smogmasterjv@gmail.com>
2026-06-10 00:44:58 +0000

    Revise header comments and version for resource module
    Updated module header comments and added version information.

--- CINS/modules/resource-arbitration/__init__.py
+++ CINS/modules/resource-arbitration/__init__.py
@@ -1,4 +1,8 @@
-"""
-Resource Arbitration Module
-Manages resource allocation across agents.
-"""
+# CINS Resource Arbitration Module
+# Cognitive Integration & Navigation System - SkyBorn Dream or Reality
+# Desertsmogtech | v0.1.0
+from .arbitrator import ResourceArbitrator
+from .resource_pool import ResourcePool
+from .allocation_policy import AllocationPolicy
+__all__ = ["ResourceArbitrator", "ResourcePool", "AllocationPolicy"]
+__version__ = "0.1.0"
